dd45e788bc15d1796d11c89023249c5d95b0ba78
narcisneacsu194 <narcisneacsu194@gmail.com>
2019-02-20 15:27:52 +0200

    Added helmet security middlewares.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(helmet.noCache());
 
 app.listen(port, () => {
   console.log(`Server started up on port ${port}`);
